2396dc6e565cd18db79a05bf159e41932698f3a3
aaaaatoz <rafa.xu.au@gmail.com>
2016-01-25 22:42:09 +1100

    add weightedquickunionuf

---
+++ src/algorithms/week1/WeightedQuickUnionUF.java
@@ -0,0 +1,28 @@
+package algorithms.week1;
+
+public class WeightedQuickUnionUF {
+
+    private int[] id;
+
+    public WeightedQuickUnionUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        while(i != id[i]) i = id[i];
+        return i;
+    }
+
+    public boolean isConnected(int p, int q){
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int rootp = root(p);
+        int rootq = root(q);
+        id[rootp] = rootq;
+    }
+}
